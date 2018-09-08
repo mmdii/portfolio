a638823abb469daf1857b72e3a689915880a8e7e
mohammad mohammadi <mohammad.mohammadii1997@gmail.com>
2018-09-08 17:30:12 +0430

    jobs page DONE git add -A!

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_data = models.DateTimeField()
     body = models.TextField()
-    image = image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
+
+
+    # ways to make new models
+    # 1 - create A Blog models
+    # 2 - add the blog app to the setting
+    # 3 - create a migration
+    # 4 - migrate
+    # 5 - add to the admin
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,14 +75,22 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfoliodb',
+#         'USER': 'postgres',
+#         'PASSWORD': '1390',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': '1390',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

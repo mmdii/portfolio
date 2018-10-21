2c6d461c79c5d1107456605f6049399843fa2ff6
mohammad mohammadi <mohammad.mohammadii1997@gmail.com>
2018-10-21 18:01:35 +0330

    update of landing page

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,6 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'portfolio/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
